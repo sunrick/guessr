b0a5e4627b46f3525637471ceda202f4ef461225
Brit Butler <redline6561@gmail.com>
2015-05-18 12:22:28 -0400

    Add part of a guessing game.

--- lib/guessr.rb
+++ lib/guessr.rb
@@ -1,5 +1,53 @@
-require "guessr/version"
+require 'pry'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'guessr/version'
+require 'guessr/init_db'
+
+## Number Guessing Game
+#* New Game
+#* Existing Game
+
+#* New Game
+#* Ask them to choose player
+#* Each turn, they can guess a number or quit
+
+#* Existing Game
+#* Each turn, they can guess a number or quit
 
 module Guessr
-  # Your code goes here...
+  class Player < ActiveRecord::Base
+  end
+
+  class Game < ActiveRecord::Base
+    def win?
+      self.answer == self.last_guess
+    end
+
+    def play
+      until self.win?
+        puts "Please guess a number (or quit with 'q'): "
+        result = gets.chomp
+        if result == 'q'
+          exit
+        else
+          self.turn(result)
+        end
+      end
+    end
+
+    def turn(guess)
+      self.update(last_guess: guess.to_i,
+                  guess_count: self.guess_count + 1)
+      if self.last_guess > self.answer
+        puts "Too high!"
+      elsif self.last_guess < self.answer
+        puts "Too low!"
+      else
+        puts "You win!"
+      end
+    end
+  end
+
+  binding.pry
 end
